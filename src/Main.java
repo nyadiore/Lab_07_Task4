81ecc8f8c34ba76652c5d6ad74e7f4aba6bb436c
nyadiore <robinn6@mail.uc.edu>
2024-10-13 22:24:56 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        // Loop that counts down by 2 from 10 to 0
+
+        for (int i = 10; i >= 0; i -= 2) {
+
+            System.out.print(i + " "); // Print each number followed by a space
+
+        }
+
+    }
+
+}
